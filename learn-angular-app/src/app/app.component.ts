38c1f8ae8800e287bf03120629d04da423345e1f
hobk1ard <hobbes.kinard@gmail.com>
2018-10-02 11:58:25 -0500

    Created some new fields and events.

--- learn-angular-app/src/app/app.component.ts
+++ learn-angular-app/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Goofy Goober';
+  name:string = 'Robert Kinard';
+  theirName:string = '';
+  theirEmail:string = '';
+
+  // constructor(name:string = '') {
+  //   this.name = name;
+  //   let arrName:string[] = name.split(' ');
+  //   this.email = this.buildEmail(arrName[0], arrName[1]);
+  // }
+
+  theirNameChange = () => {
+    console.log(this.name);
+    let arrName:string[] = this.theirName.split(' ');
+    this.theirEmail = this.buildEmail(arrName[0], arrName[1]);
+  };
+
+  private buildEmail = function(firstName:string = '', lastName:string = '') {
+    if(firstName) {
+      return `${firstName}${lastName ? '.' + lastName : ""}@argodata.com`;
+    }
+    else {
+      return "";
+    }
+  }
+
+  email:string = this.buildEmail('Robert', 'Kinard');
 }
